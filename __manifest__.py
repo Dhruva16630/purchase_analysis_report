8a67e065d75eea573d5845f2c9fe5c85a5934c18
Dhruva16630 <dhruva6kumar1@gmail.com>
2026-09-25 12:57:39 +0530

    Project_setup

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name":"Purchase Analysis Report",
+    "version":"1.0",
+    "depends":[
+        "purchase"
+    ],
+    "data":[],
+    "installable":True
+}
---
+++ report/purchase_analysis_report.py
@@ -0,0 +1,7 @@
+from odoo import models,fields,tools
+
+class PurchaseAnalysisReport(models.Model):
+    _name = "purchase.analysis.report"
+    _description = "Purchase Analysis report"
+    _auto = False
+    
